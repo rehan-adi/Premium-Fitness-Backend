8b38614e20ebc0b9eaf629cd954797dfff755709
Rehan <rehanalire52@gmail.com>
2024-05-21 13:15:38 +0530

    add user model to code base and also add .gitignore file

---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    password: {
+        required: true,
+        type: String
+    }
+});
+
+const userModel = mongoose.model("userModel", userSchema);
+
+export default userModel;
