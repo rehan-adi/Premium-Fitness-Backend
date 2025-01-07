0e167339a471a6ad4c3819751a31868b389d6d2f
Rehan <rehanalire52@gmail.com>
2025-01-07 22:45:49 +0530

    chore: clean slate - removed all old code to start fresh

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register, login } from "../controllers/auth.controllers.js";
-
-const authRouter = express.Router();
-
-authRouter.post("/reginter", register);
-authRouter.post("/login", login);
-
-export default authRouter;
--- routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import contactController from '../controllers/contact.controller.js';
-
-const contactRouter = express.Router();
-
-contactRouter.post("/query", contactController)
-
-export default contactRouter;
