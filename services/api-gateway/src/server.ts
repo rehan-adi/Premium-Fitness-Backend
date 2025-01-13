3447de5019ce4149e0e11c88f9b7158cc1d8fe4a
Rehan <rehanalire52@gmail.com>
2025-01-13 13:47:03 +0530

    feat: add security and performance enhancements with helmet and compression, implement rate limiting for auth and order routes

--- services/api-gateway/src/server.ts
+++ services/api-gateway/src/server.ts
@@ -1,15 +1,20 @@
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 import bodyParser from "body-parser";
+import compression from "compression";
 import proxy from "express-http-proxy";
 import express, { Request, Response } from "express";
+import { authLimiter, orderLimiter } from "./utils/limiters";
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 // middleware's
 server.use(cors());
+server.use(helmet());
 server.use(morgan("dev"));
+server.use(compression());
 server.use(bodyParser.json());
 
 // proxy middleware for User Service (handles both /auth and /payment-methods)
@@ -29,15 +34,15 @@ const orderServiceProxy = proxy("http://localhost:2000", {
 });
 
 // user service routes
-server.use("/api/auth", userServiceProxy);
+server.use("/api/auth", authLimiter, userServiceProxy);
 server.use("/api/payment-methods", userServiceProxy);
 
 // order service routes
-server.use("/api/order", orderServiceProxy);
+server.use("/api/order", orderLimiter, orderServiceProxy);
 
 // health check route
 server.get("/", (req: Request, res: Response) => {
-  res.send("API Gateway is running");
+  res.json({ success: true, message: "API Gateway is running" });
 });
 
 // fallback route for unmatched requests
